d45326749ec4936dfc7f1a743de44222fab5c952
Plaban Biswas <plabanbiswas96@outlook.com>
2026-02-13 16:29:44 -0500

    - UPDATED: Minimum duration for scene transition

--- Unity_Project/ModularPrototypes/Assets/Main/Scripts/Scene/SceneLoader.cs
+++ Unity_Project/ModularPrototypes/Assets/Main/Scripts/Scene/SceneLoader.cs
@@ -58,7 +58,7 @@ namespace ModularPrototypes.Scene
         {
             SceneTransition transition = _transitions.First(element => element.name.Equals(transitionName));
 
-            yield return transition.AnimateTransitionIn();
+            yield return transition.AnimateTransitionIn(0.5f);
 
             _progressBar.gameObject.SetActive(true);
 
@@ -75,7 +75,7 @@ namespace ModularPrototypes.Scene
             scene.allowSceneActivation = true;
             _progressBar.gameObject.SetActive(false);
 
-            yield return transition.AnimateTransitionOut();
+            yield return transition.AnimateTransitionOut(0.5f);
         }
 
         public void UnloadScene(string sceneToUnload)
@@ -99,77 +99,3 @@ namespace ModularPrototypes.Scene
         }
     }
 }
-
-/*
- * using Minimalist.Scene.Transition;
-
-using System.Collections;
-using System.Linq;
-
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace Minimalist.Manager
-{
-    public class SceneManager : MonoBehaviour
-    {
-        public static SceneManager Instance { get; private set; }
-
-        [SerializeField] private GameObject _transitionContainer;
-        [SerializeField] private SceneTransition[] _transitions;
-
-
-        [SerializeField] private Slider _progressBar;
-
-        private void Awake()
-        {
-            if (Instance == null) // Singleto
-            {
-                Instance = this;
-                DontDestroyOnLoad(Instance);
-            }
-            else
-            {
-                Destroy(this);
-            }
-        }
-
-        private void Start()
-        {
-            _transitions = _transitionContainer.GetComponentsInChildren<SceneTransition>();
-        }
-
-        public void LoadScene(string sceneName, string transitionName)
-        {
-            StartCoroutine(LoadSceneAsync(sceneName, transitionName));
-        }
-
-        private IEnumerator LoadSceneAsync(string sceneName, string transitionName)
-        {
-            SceneTransition transition = _transitions.First(element => element.name.Equals(transitionName));
-
-            AsyncOperation scene = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
-            scene.allowSceneActivation = false;
-
-            yield return transition.AnimateTransitionIn();
-
-            _progressBar.gameObject.SetActive(true);
-
-            do
-            {
-                //_progressBar.value = scene.progress;
-                _progressBar.value = Mathf.SmoothStep(_progressBar.value, scene.progress, .05f);
-                yield return null;
-
-            } while (scene.progress < .9f || _progressBar.value < 0.875f);
-            //} while (scene.progress < 0.9f);
-            //} while (_progressBar.value < 0.9f);
-
-            scene.allowSceneActivation = true;
-            _progressBar.gameObject.SetActive(false);
-
-            yield return transition.AnimateTransitionOut( );
-        }
-    }
-}
- */
